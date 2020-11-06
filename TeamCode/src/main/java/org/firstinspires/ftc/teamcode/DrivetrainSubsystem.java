1a42a936b58f9e2e12c2e6a8a131ea1926bc8ac9
jfletcher <jfletcher@lsc.k12.in.us>
2020-11-06 08:43:23 -0500

    update mecanum drive code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivetrainSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivetrainSubsystem.java
@@ -1,4 +1,80 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class DrivetrainSubsystem {
+    DcMotor frontLeft, frontRight, backLeft, backRight;
+
+    public DrivetrainSubsystem(HardwareMap hardwareMap) {
+        frontRight = hardwareMap.get(DcMotor.class, "FR");
+        frontLeft = hardwareMap.get(DcMotor.class, "FL");
+        backLeft = hardwareMap.get(DcMotor.class, "BL");
+        backRight = hardwareMap.get(DcMotor.class, "BR");
+
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void stopDriving() {
+        setMotors(0, 0, 0, 0);
+    }
+
+    public void setMotors(double FL, double BL, double FR, double BR) {
+        frontLeft.setPower(FL);
+        frontRight.setPower(FR);
+        backLeft.setPower(BL);
+        backRight.setPower(BR);
+    }
+
+    public void mecanumDrive_Cartesian(double x, double y, double rotation) {
+        double wheelSpeeds[] = new double[4];
+
+        x = deadband(x);
+        y = deadband(y);
+        rotation = deadband(rotation);
+
+        x = x * x * x;
+        y = y * y * y;
+        rotation = rotation * rotation * rotation;
+
+        wheelSpeeds[0] = -x + y + rotation;
+        wheelSpeeds[1] = x + y + rotation;
+        wheelSpeeds[2] = x + y - rotation;
+        wheelSpeeds[3] = -x + y - rotation;
+
+        wheelSpeeds = normalize(wheelSpeeds);
+
+        setMotors(wheelSpeeds[0], wheelSpeeds[1], wheelSpeeds[2], wheelSpeeds[3]);
+
+    }
+
+    private double[] normalize(double[] wheelSpeeds) {
+        double maxMagnitude = Math.abs(wheelSpeeds[0]);
+
+        for (int i = 1; i < wheelSpeeds.length; i++) {
+            double magnitude = Math.abs(wheelSpeeds[i]);
+            if (magnitude > maxMagnitude) {
+                maxMagnitude = magnitude;
+            }
+        }
+
+        if (maxMagnitude > 1.0) {
+            for (int i = 0; i < wheelSpeeds.length; i++) {
+                wheelSpeeds [i] /= maxMagnitude;
+            }
+        }
+        return wheelSpeeds;
+    }
+
+    private double deadband(double x) {
+        if (Math.abs(x) < .1) {
+            x = 0;
+        }
+        return x;
+    }
+
 }
